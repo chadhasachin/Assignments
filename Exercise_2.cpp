e8e45e2205ef176926d7382d2584d0d2966ba28a
chadhasachin <sachins.chadha@gmail.com>
2019-04-07 23:11:37 +0200

    Update Exercise_2.cpp

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -2,62 +2,20 @@
 #include <iostream>
 #include <memory>
 
-template <typename T, typename P>
-class NumericType 
-{
-public:
-    explicit NumericType(const T& value) : value_{value}
-    {
-        if(std::is_arithmetic<T>::value && value_ < 0)
-            throw std::range_error("Only arithmetic non negative values are supported");
-    }
-
-    explicit NumericType(T&& value)      : value_{std::move(value)}
-    {
-        if(std::is_arithmetic<T>::value && value_ < 0)
-            throw std::range_error("Only arithmetic non negative values are supported");
-    }
-
-    NumericType(const NumericType& rhs) : value_{rhs.value_}
-    {}
-
-    NumericType& operator=(const NumericType& rhs)
-    {
-        if(&rhs == this)
-            return *this;
-
-        value_ = rhs.value_;
-        return *this; 
-    }
-
-    bool operator==( const NumericType& rhs ) const
-    {
-        if(&rhs == this)
-            return true;
-        return (value_ == rhs.value_);        
-    }
-
-    T& get() { return value_; }
-    const T & get() const {return value_; }
-
-private:
-    T value_;    
-};
 
 template <typename T>
 class Sequence 
 {
 public:
-    using sequence_size = NumericType<T, struct sequence_size>;
-
     Sequence() = delete;
-    explicit Sequence(sequence_size count) : size_(count), memory_(std::make_unique<T[]>(count.get()))
+
+    explicit Sequence(std::size_t count) : size_(count), memory_(std::make_unique<T[]>(count))
     {}
 
     explicit Sequence(const Sequence& rhs) : size_(rhs.size_)
     {
-        memory_ = std::make_unique<T[]>(size_.get());
-        std::copy(&rhs.memory_[0], &rhs.memory_[0] + size_.get(), &memory_[0]);
+        memory_ = std::make_unique<T[]>(size_);
+        std::copy(&rhs.memory_[0], &rhs.memory_[0] + size_, &memory_[0]);
     }
 
     Sequence& operator=(const Sequence& rhs)
@@ -65,13 +23,13 @@ public:
         if(&rhs == this)
             return *this;
 
-        if(size_.get() != rhs.size_.get())
+        if(size_ < rhs.size_)
         {
-            memory_.reset(new T[rhs.size_.get()]);
+            memory_.reset(new T[rhs.size_]);
             size_ = rhs.size_;
         }
 
-        std::copy(&rhs.memory_[0], &rhs.memory_[0] + size_.get(), &memory_[0]);
+        std::copy(&rhs.memory_[0], &rhs.memory_[0] + size_, &memory_[0]);
         return *this;
     }
 
@@ -80,37 +38,49 @@ public:
         if(&rhs == this)
             return true;
         
-        return (std::equal(&rhs.memory_[0], &rhs.memory_[0] + size_.get(), &memory_[0]) && size_ == rhs.size_);
+        return (std::equal(&rhs.memory_[0], &rhs.memory_[0] + size_, &memory_[0]) && size_ == rhs.size_);
     }
 
-    T operator[](std::size_t index) const
+    T& operator[](std::size_t index) const
     {
         return memory_[index];
     }
 
-    T at(std::size_t index) const
+    T& at(std::size_t index) const
     {
-        if(index < 0 || index > size_.get())
-            return 0;
+        static T ret = 0;
+
+        if(index > size_)
+            return ret;
             
         return memory_[index];
     }
+
     ~Sequence() = default; // Unique pointer will be destroyed
+
+    std::size_t get_size() const
+    {
+        return size_;
+    }
 
 private:
     std::unique_ptr<T[]> memory_;
-    sequence_size size_;    
-    
+    std::size_t size_;   
 };
 
-template <typename T>
 class IntegerSequence 
 {
 public:
-    using sequence_size = typename Sequence<T>::sequence_size;
 
-    explicit IntegerSequence(sequence_size size) : s_(size)
-    {}
+    explicit IntegerSequence(std::size_t size) 
+    try : s_(size)
+    {
+        std::cout << size;
+    } 
+    catch (std::bad_array_new_length&)
+    {
+        throw;
+    }
 
     IntegerSequence(const IntegerSequence& rhs) : s_(rhs.s_)
     {}
@@ -125,18 +95,33 @@ public:
         return (s_ == rhs.s_);
     }
 
-    T operator[](std::size_t index) const
+    std::uint32_t& operator[](std::size_t index) const
     {
         return s_[index];
     }
 
-    T at(std::size_t index) const
+    std::uint32_t& at(std::size_t index) const
     {
         return s_.at(index);
     }
+
+    std::size_t get_size() const
+    {
+        return s_.get_size();
+    }
+
 private:
-    Sequence<T> s_;
+    Sequence<std::uint32_t> s_;
 };
 
+IntegerSequence operator+ (const IntegerSequence& lhs, const IntegerSequence& rhs)
+{
+    IntegerSequence ret( lhs.get_size() + rhs.get_size() );
+
+    ret = lhs;
+    std::copy(&rhs[0], &rhs[0] + rhs.get_size(), &ret[lhs.get_size()]);
+
+    return ret;
+}
 int main()
 {}
