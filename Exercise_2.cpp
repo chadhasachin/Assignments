904453534458907818c1a281b6efff99b39a3b7a
chadhasachin <sachins.chadha@gmail.com>
2019-04-08 21:57:05 +0200

    Update Exercise_2.cpp

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -8,29 +8,43 @@ class Sequence
 {
 public:
     Sequence() = delete;
-    //! using std::size_t as point 1 says the that the count is non negative. If this needs to be enforces
-    //! I would prefer Strong type, similar to Exercise 1.
-    explicit Sequence(std::size_t count) : size_(count), memory_(std::make_unique<T[]>(count))
+
+    explicit Sequence(std::size_t count) : size_(count), memory_(new T[count])
     {}
 
     explicit Sequence(const Sequence& rhs) : size_(rhs.size_)
     {
-        memory_ = std::make_unique<T[]>(size_);
+        memory_ = new T[size_];
         std::copy(&rhs.memory_[0], &rhs.memory_[0] + size_, &memory_[0]);
     }
 
-    Sequence& operator=(const Sequence& rhs)
+    ~Sequence() 
+    {
+        delete [] memory_;
+    }
+
+    Sequence& operator=(const Sequence& rhs) 
     {
         if(&rhs == this)
             return *this;
 
+        
         if(size_ < rhs.size_)
         {
-            memory_.reset(new T[rhs.size_]);
+            T * temp; 
+            temp = (new T[rhs.size_]); // Maintain the exception gurantee, only change state if new succeeds.
+            std::copy(&rhs.memory_[0], &rhs.memory_[0] + size_, &temp[0]);            
+
+            delete[] memory_;
             size_ = rhs.size_;
+            memory_= temp;
+        }
+        else
+        {
+            std::copy(&rhs.memory_[0], &rhs.memory_[0] + size_, &memory_[0]);
         }
 
-        std::copy(&rhs.memory_[0], &rhs.memory_[0] + size_, &memory_[0]);
+
         return *this;
     }
 
@@ -47,17 +61,13 @@ public:
         return memory_[index];
     }
 
-    T& at(std::size_t index) const
+    T at(std::size_t index) const
     {
-        static T ret = 0;
-
         if(index > size_)
-            return ret;
+            return 0;
             
         return memory_[index];
     }
-
-    ~Sequence() = default; // Unique pointer will be destroyed
 
     std::size_t get_size() const
     {
@@ -65,7 +75,7 @@ public:
     }
 
 private:
-    std::unique_ptr<T[]> memory_;
+    T* memory_;
     std::size_t size_;   
 };
 
@@ -73,7 +83,7 @@ class IntegerSequence
 {
 public:
 
-    using sequence_data_type = std::int32_t;
+    using sequnce_data_type = std::int32_t;
     explicit IntegerSequence(std::size_t size) 
     try : s_(size)
     {} 
@@ -95,12 +105,12 @@ public:
         return (s_ == rhs.s_);
     }
 
-    sequence_data_type& operator[](std::size_t index) const
+    sequnce_data_type& operator[](std::size_t index) const
     {
         return s_[index];
     }
 
-    sequence_data_type& at(std::size_t index) const
+    sequnce_data_type at(std::size_t index) const
     {
         return s_.at(index);
     }
@@ -111,7 +121,7 @@ public:
     }
 
 private:
-    Sequence<sequence_data_type> s_;
+    Sequence<sequnce_data_type> s_;
 };
 
 IntegerSequence operator+ (const IntegerSequence& lhs, const IntegerSequence& rhs)
@@ -181,18 +191,6 @@ int main()
     auto sequence_d = sequence_a + sequence_b;  // 30 elements with contents of a followed by b
 
     ret &= (sequence_c == sequence_d);
-
-    // Strong exception gurantee will be held due to use of unique pointers. Try allocating a very big array.
-
-    try 
-    {
-        auto sequence_too_big = create_sequence(UINT64_MAX, 0);
-    } 
-    catch(std::bad_array_new_length& e)
-    {
-        std::cout << "Caught: " << e.what() << std::endl;
-        ret &= (sequence_c == sequence_d);
-    }
 
     std::cout << std::boolalpha;
     std::cout << ret << std::endl;
